1795734f737c14f9722892533b06202ee71bb1f0
Roman Tereshchenko <romante@intern.blackwood.gg>
2020-06-19 11:08:15 +0300

    test failed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,7 @@ test("renders learn react link", () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("failed test", () => {
+  expect(true).toBe(false);
+});
